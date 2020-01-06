03455befc0ac6d9e8c82849df3c982f0d7c6f96b
sayantanHack <sayantandatta97@gmail.com>
2020-01-06 08:00:25 +0530

    added inital functions & added Gmap API

--- rest.js
+++ rest.js
@@ -1,1 +1,10 @@
-alert("Hiii!!");
+console.log("Hiii!!");
+var map;
+
+function initMap(){
+    var options = {
+        zoom=8,
+        center:{lat:22.6052248,lng:88.38067 }
+    }
+    map = new google.maps.Map(document.getElementById('map'),options);
+}
