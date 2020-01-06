a0eb4298e7375e3867c788fae79d794ea92e5662
sayantanHack <sayantandatta97@gmail.com>
2020-01-06 08:20:59 +0530

    fixing some issues

--- rest.js
+++ rest.js
@@ -3,7 +3,7 @@ var map;
 
 function initMap(){
     var options = {
-        zoom=8,
+        zoom:8,
         center:{lat:22.6052248,lng:88.38067 }
     }
     map = new google.maps.Map(document.getElementById('map'),options);
