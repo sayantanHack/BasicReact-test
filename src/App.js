e93eeff058e6ff47a83a8ade77d08014af2dd405
sayantanHack <sayantandatta97@gmail.com>
2020-02-23 23:28:46 +0530

    Added the gMap element. WIth few errors.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { GooogleMapReact, withScriptjs, withGoogleMap } from "google-map-react";
+
+function Map() {
+  return (
+    <GooogleMapReact
+      defaultZoom={10}
+      defaultCenter={{ lat: 22.572645, lng: 88.363892 }}
+    />
+  );
+}
+
+let MapWrapElement = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Resturant Review App
+      </h1>
+      <mapWrapElement googlemapurl={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&KEY="AIzaSyBo107zHkcIgXHMOTCPw-iONo4I7Ibq0ZI"`
+      }
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
